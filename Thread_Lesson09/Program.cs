4bdfe5b82af5d862f20a6561ff3966f1f3ed8d58
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-06-23 00:24:46 +0500

    create project

---
+++ Thread_Lesson09/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Thread_Lesson09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thread Lesson 09. Homeworks.");
+        }
+    }
+}
